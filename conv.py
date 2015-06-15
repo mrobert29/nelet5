5dce72c159542490b561b4c36a72eecbfd08f7fc
Pikkendorff <guillaume.soulie@gmail.com>
2015-06-15 22:45:13 +0200

    fixings bugs

--- conv.py
+++ conv.py
@@ -34,8 +34,8 @@ from theano.tensor.signal import downsample
 from theano.tensor.nnet import conv
 import cPickle
 
-from logistic_sgd import BinaryLogisticRegression,LogisticRegression, load_data
-from mlp import HiddenLayer, Binary_HiddenLayer
+from logistic_sgd import LogisticRegression, load_data
+from mlp import HiddenLayer
 import matplotlib.pyplot as plt
 from test import test
 
@@ -115,71 +115,6 @@ class LeNetConvPoolLayer(object):
         # store parameters of this layer
         self.params = [self.W, self.b]
 
-class BinaryLeNetConvPoolLayer(object):
-    """Pool Layer of a convolutional network """
-
-    def __init__(self,W,b, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
-        """
-        Allocate a LeNetConvPoolLayer with shared variable internal parameters.
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.dtensor4
-        :param input: symbolic image tensor, of shape image_shape
-
-        :type filter_shape: tuple or list of length 4
-        :param filter_shape: (number of filters, num input feature maps,
-                              filter height, filter width)
-
-        :type image_shape: tuple or list of length 4
-        :param image_shape: (batch size, num input feature maps,
-                             image height, image width)
-
-        :type poolsize: tuple or list of length 2
-        :param poolsize: the downsampling (pooling) factor (#rows, #cols)
-        """
-
-        assert image_shape[1] == filter_shape[1]
-        self.input = input
-
-        # there are "num input feature maps * filter height * filter width"
-        # inputs to each hidden unit
-        fan_in = numpy.prod(filter_shape[1:])
-        # each unit in the lower layer receives a gradient from:
-        # "num output feature maps * filter height * filter width" /
-        #   pooling size
-        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /
-                   numpy.prod(poolsize))
-        # initialize weights with random weights
-        W_bound = numpy.sqrt(6. / (fan_in + fan_out))
-        self.W = 0.01*(W>0)-0.01*(W<0)
-        # initialize the baises b as a vector of n_out 0s
-        self.b = b
-
-        # convolve input feature maps with filters
-        conv_out = conv.conv2d(
-            input=input,
-            filters=self.W,
-            filter_shape=filter_shape,
-            image_shape=image_shape
-        )
-
-        # downsample each feature map individually, using maxpooling
-        pooled_out = downsample.max_pool_2d(
-            input=conv_out,
-            ds=poolsize,
-            ignore_border=True
-        )
-
-        # add the bias term. Since the bias is a vector (1D array), we first
-        # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
-        # thus be broadcasted across mini-batches and feature map
-        # width & height
-        self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
-
-        # store parameters of this layer
-        self.params = [self.W, self.b]
 
 
 def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
@@ -343,27 +278,9 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
         }
     )
 
-    test_binary_model = theano.function(
-        [index],
-        layer3_binary.errors(y),
-        givens={
-            x: test_set_x[index * batch_size: (index + 1) * batch_size],
-            y: test_set_y[index * batch_size: (index + 1) * batch_size]
-        }
-    )
-
     validate_model = theano.function(
         [index],
         layer3.errors(y),
-        givens={
-            x: valid_set_x[index * batch_size: (index + 1) * batch_size],
-            y: valid_set_y[index * batch_size: (index + 1) * batch_size]
-        }
-    )
-
-    validate_binary_model = theano.function(
-        [index],
-        layer3_binary.errors(y),
         givens={
             x: valid_set_x[index * batch_size: (index + 1) * batch_size],
             y: valid_set_y[index * batch_size: (index + 1) * batch_size]
@@ -457,10 +374,10 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
                 # plt.hist(layer3.W.get_value(), 50, normed=1, facecolor='g', alpha=0.75)
                 # plt.show()
                 # compute zero-one loss on validation set
-                f = file(titre+'-p', 'wb')
+                f = file(titre+'sauv-p', 'wb')
                 cPickle.dump(params, f, protocol=cPickle.HIGHEST_PROTOCOL)
                 f.close()
-                this_binary_validation_loss=test(titre+'-p',dep)
+                this_binary_validation_loss=test(titre+'sauv-p',dep)
 
                 validation_losses = [validate_model(i) for i
                                      in xrange(n_valid_batches)]
@@ -507,7 +424,7 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
                 fichier.write('%s - %i - epoch %i - minibatch %i/%i - c : %f - a %f %s - t %f - '
                             't %f %%  - bt  %f %% \n' %
                             (titre,dep,epoch, minibatch_index + 1, n_train_batches,this_validation_loss,alpha,alpha_status,
-                            (time.clock()-start_time)/60,test_score * 100.,binary_test_score*100))
+                            (time.clock()-start_time)/60,test_score * 100.,this_binary_validation_loss*100))
 
                 fichier.close()
 
